119e2360c9c9e2cd4d53cde57fd318821671543b
angeloren92 <angeloren92@gmail.com>
2026-02-16 14:25:22 +0100

    add human code

---
+++ js/functions.js
@@ -0,0 +1,2 @@
+//2.generare numeri casuali: da 1 a 50, ps non deve dare numeri doppi
+
--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log('test')
+/*
+-agganciamo i nodi della dom
+
+1 agganciare i nodi della dom
+2 generare numeri casuali: da 1 a 50, ps non deve dare numeri doppi
+3 timer di 30 secondi per i numeri da visualizzare
+4 visualizzazione form con inputs
+5 evento quando clicca conferma
+6 validazione numeri inseriti //è il bonus per ultimo
+7 confronto numeri generati e numeri inseriti da utente
+8 visualizzazione risultato in html
+*/
