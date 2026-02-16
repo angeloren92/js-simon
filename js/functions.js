93b1e0d59aba0b03005ebe6c5679079855d97207
angeloren92 <angeloren92@gmail.com>
2026-02-16 15:22:29 +0100

    add function getRandomNums

--- js/functions.js
+++ js/functions.js
@@ -1,2 +1,13 @@
 //2.generare numeri casuali: da 1 a 50, ps non deve dare numeri doppi
+/**
+ * genera una cinquina di numeri compresi da 1 a 50
+ * @returns restituisce un oggetto
+ */
+function getRandomNums() {
+    const randomNums = new Set();
+    while (randomNums.size != 5) {randomNums.add(Math.floor(Math.random() * 50) + 1)}
+    return randomNums;
+}
+const randomNum = getRandomNums()
+console.log(randomNum)
 
