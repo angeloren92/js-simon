a430ef642eb9aafca7e612675e5bbb6ade47c00b
angeloren92 <angeloren92@gmail.com>
2026-02-16 16:54:20 +0100

    add setTimeout

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-//2.generare numeri casuali: da 1 a 50, ps non deve dare numeri doppi
+//generare numeri casuali: da 1 a 50, ps non deve dare numeri doppi
 /**
  * genera una cinquina di numeri compresi da 1 a 50
  * @returns restituisce un oggetto
@@ -11,20 +11,32 @@ function getRandomNums() {
 const randomNums = getRandomNums()
 console.log(randomNums)
 
-//3.timer di 30 secondi per i numeri da visualizzare
-const timer = setTimeout(function(){
-    //da rivedere le istruzioni da eseguire
-
-}, 3000)
+const numbersListEl = document.getElementById('numbers-list');
+const answersFormEl = document.getElementById('answers-form')
 
 //Inseriamo un nuovo elemento html per ogni elemento dell'oggetto
-
-function logRandomNums(obj) {
-    
-}
-
 randomNums.forEach(randomNum => {
-    const numbersListEl = document.getElementById('numbers-list');
-    numbersListEl.insertAdjacentHTML('beforeend', `<li>${randomNum}</li>`);
+    numbersListEl.innerHTML += `<li>${randomNum}</li>`;
     console.log(randomNum);
 });
+
+//timer di 30 secondi per lo switch di visualizzazione, da numeri random generati a form per l'inserimento input
+const timer = setTimeout(function() {
+    //da rivedere le istruzioni da eseguire
+    numbersListEl.classList.add('d-none')
+    answersFormEl.classList.remove('d-none')
+}, 3000);
+
+//Agganciamo i nodi degli inputes
+const inputEl = document.querySelectorAll('input');
+const buttonEl = document.querySelector('button');
+const buttonValues = [];
+console.log(inputEl, buttonEl);
+
+buttonEl.addEventListener('click', function(e){
+    e.preventDefault();
+    buttonValues.forEach(value =>)
+    
+})
+
+
